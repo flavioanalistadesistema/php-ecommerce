41363efb966d413b9b71e9401107566e368ba51f
MrRobotProgrammer <flavio.santos@pravaler.com.br>
2020-06-06 11:13:19 -0300

    incluindo classes autoload

--- namespace/config.php
+++ namespace/config.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function($nameClass){
-    
+
     $className = str_replace('\\', '/', $nameClass);
     $dirName = "Class";
     $filename = $dirName . DIRECTORY_SEPARATOR . $className . ".php";
