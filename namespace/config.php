2b2c41026347c3d0ba7bfae4ee2f77afda305aa9
MrRobotProgrammer <flavio.santos@pravaler.com.br>
2020-06-06 13:45:22 -0300

    Incluindo REDIRECTORY_SEPARETOR

--- namespace/config.php
+++ namespace/config.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($nameClass){
 
-    $className = str_replace('\\', '/', $nameClass);
+    $className = str_replace('\\', DIRECTORY_SEPARATOR , $nameClass);
     $dirName = "Class";
     $filename = $dirName . DIRECTORY_SEPARATOR . $className . ".php";
     
